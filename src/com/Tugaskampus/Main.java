4195f3b7ea58186b76527d176bd075c6654a9525
Mhaemnn <ismuhaimin1@gmail.com>
2022-03-24 17:59:06 +0700

    delate

--- src/com/Tugaskampus/Main.java
+++
@@ -1,9 +0,0 @@
-package com.Tugaskampus;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// menampilkan hello word ke console
-        System.out.println("Hello World!");
-    }
-}
