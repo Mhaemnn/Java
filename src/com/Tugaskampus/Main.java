6456ebd922d6d044c69bc77e2be8b2a6f99cb925
Mhaemnn <ismuhaimin1@gmail.com>
2022-03-24 15:36:39 +0700

    add folders

---
+++ src/com/Tugaskampus/Main.java
@@ -0,0 +1,9 @@
+package com.Tugaskampus;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// menampilkan hello word ke console
+        System.out.println("Hello World!");
+    }
+}
