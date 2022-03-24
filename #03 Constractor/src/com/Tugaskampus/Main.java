87a7b70b8d00336954ee40bd86afb58784155dda
Mhaemnn <ismuhaimin1@gmail.com>
2022-03-24 20:44:44 +0700

    modifikasi

--- #03 Constractor/src/com/Tugaskampus/Main.java
+++ #03 Constractor/src/com/Tugaskampus/Main.java
@@ -1,9 +1,3 @@
-/*
-Pemilik : Muhaemin
-Pertemuan ke - 3 [Constructor]
-Constructor adalah method khusus yang akan di eksekusi saat pembuataan object (instance)
-*/
-
 package com.Tugaskampus;
 
 //class tampa CONSTRUCTOR
@@ -14,17 +8,30 @@ class Mahasiswa{
     String IPK;
 
 
-    //ini adalah constructor
-    Mahasiswa(){
-        System.out.println("ini adalah consrtuctor");
+    //ini adalah constructor tampa parameter
+//    Mahasiswa(){
+//
+//        System.out.println("ini adalah consrtuctor");
+//    }
+//}
+    //contructor dengan parameter
+    Mahasiswa(String inputNama, String inputJurusan){
+        inputNama = inputNama;
+        inputJurusan = inputJurusan;
+
+        System.out.println(inputNama);
+        System.out.println(inputJurusan);
     }
 }
-
 public class Main {
 
     public static void main(String[] args) {
 	// object (instance) dari Mahasiswa
-        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa mhs1 = new Mahasiswa("samsudin", "teknik terbang");
+        Mahasiswa mhs2 = new Mahasiswa("rama", "teknik propokasi");
+
+
+
 //        mhs1.nama = "mamot";
 //        mhs1.NIM = "849580";
 //        mhs1.jurusan = "teknik tidur";
