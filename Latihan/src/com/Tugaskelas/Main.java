fed10ee77f28177ff037c28927216e8e65c78e1d
Mhaemnn <ismuhaimin1@gmail.com>
2022-04-16 06:17:21 +0700

    delate folder Muhaemin

--- Latihan/src/com/Tugaskelas/Main.java
+++ Latihan/src/com/Tugaskelas/Main.java
@@ -1,61 +1,64 @@
 package com.Tugaskelas;
-// player
-class Player{
-    String name;
-    double health;
+// pemain
+class Pemain{
+    String nama;
+    double nyawa;
     int level;
     // object member
-    Weapon weapon;
-    Armor armor;
+    Senjata senjata;
+    Kostum kostum;
 
-    Player(String name, double health){
-        this.name = name;
-        this.health = health;
+    Pemain(String nama, double nyawa){
+        this.nama = nama;
+        this.nyawa = nyawa;
     }
 
-    void equipWeapon(Weapon weapon){
-        this.weapon = weapon;
+    void equipSenjata(Senjata senjata){
+        this.senjata = senjata;
     }
 
-    void equipArmor(Armor armor){
-        this.armor = armor;
+    void equipKostum(Kostum kostum){
+        this.kostum = kostum;
     }
 
-    void display(){
-        System.out.println("\nName : " + this.name);
-        System.out.println("Health : " + this.health + " hp");
-        this.weapon.display();
-        this.armor.display();
+    void tampil(){
+        System.out.println("\nNama : " + this.nama);
+        System.out.println("Nyawa  : " + this.nyawa + " hp");
+        this.senjata.tampil();
+        this.kostum.pemain();
     }
 }
 
 // senjata
-class Weapon{
-    double attackPower;
-    String name;
+class Senjata{
+    double kekuatanSerang;
+    String nama;
 
-    Weapon(String name, double attackPower){
-        this.name = name;
-        this.attackPower = attackPower;
+    Senjata(String nama, double kekuatanSerang){
+        this.nama = nama;
+        this.kekuatanSerang = kekuatanSerang;
     }
 
-    void display(){
-        System.out.println("Weapon : " + this.name + " , attack : " + this.attackPower);
+    void tampil(){
+        System.out.println("Senjata : " + this.nama + " , Serangan : " + this.kekuatanSerang);
     }
 }
 
-// armor
-class Armor{
-    double defencePower;
-    String name;
+// kostum
+class Kostum{
+    double kekuatanBertahan;
+    String nama;
 
-    Armor(String name, double defencePower){
-        this.name = name;
-        this.defencePower = defencePower;
+    Kostum(String nama, double kekuatanBertahan){
+        this.nama = nama;
+        this.kekuatanBertahan = kekuatanBertahan;
     }
 
-    void display(){
-        System.out.println("Armor : " + this.name + " , defence : " + this.defencePower);
+    void tampil(){
+        System.out.println("Kostum : " + this.nama + " , Bertahan : " + this.kekuatanBertahan);
+    }
+
+    public void pemain() {
     }
 }
 
@@ -63,26 +66,26 @@ public class Main{
 
     public static void main(String[] args) {
 
-        // membuat object player
-        Player player1 = new Player("ucup",100);
-        Player player2 = new Player("otong",50);
+      //membuat objek pemain
+        Pemain pemain1 = new Pemain("Doni", 90);
+        Pemain pemain2 = new Pemain("Rio", 50);
 
-        // membuat object weapon
-        Weapon pedang = new Weapon("pedang",15);
-        Weapon ketapel = new Weapon("ketapel",1);
+        //membuat senjata objek
+        Senjata kapak = new Senjata("Kapak", 60);
+        Senjata pukulan = new Senjata("pukulan", 10);
 
-        // membuat object armor
-        Armor bajuBesi = new Armor("baju besi",10);
-        Armor kaos = new Armor("kaos",0);
+        //membuat kostum objek
+        Kostum spiderman = new Kostum("spiderman",10);
+        Kostum hulk = new Kostum("hulk",50);
 
-        // player 1
-        player1.equipWeapon(pedang);
-        player1.equipArmor(bajuBesi);
-        player1.display();
+        //pemain1
+        pemain1.equipKostum(spiderman);
+        pemain1.equipSenjata(kapak);
+        pemain1.tampil();
 
-        // player 2
-        player2.equipWeapon(ketapel);
-        player2.equipArmor(kaos);
-        player2.display();
+        //pemain2
+        pemain2.equipKostum(hulk);
+        pemain2.equipSenjata(pukulan);
+        pemain2.tampil();
     }
 }
