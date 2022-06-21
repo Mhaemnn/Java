a1f0b8d4a6be0f307ff37223dcab7a1dc031ceb5
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-21 22:23:35 +0700

    modifikasi

---
+++ pertemuan 02/src/com/javacampus/Main.java
@@ -0,0 +1,31 @@
+package com.javacampus;
+
+/*
+//study variables
+int myNum = 5;
+float myFloatNum = 5.99f;
+boolean myBool = true;
+char myLetter = 'D';
+String myText = "Hello";
+ */
+public class Main {
+    public static void main(String[] args){
+        //string
+        String lastName = "Muahemin";
+        String firtsName = " Iskandar";
+        String fullName = (lastName + firtsName);
+        System.out.println(fullName);
+
+        System.out.println("=====================");
+
+        //int
+        int a = 56;
+        int b = 89;
+        int c = 8;
+        System.out.println("Nilai dari hasil di atas = "+a + b * c );
+        //float
+        System.out.println();
+    }
+
+
+}
