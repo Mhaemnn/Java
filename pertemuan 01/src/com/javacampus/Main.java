3b3cf8e8c6c59a4770c494711f1b2b1766a59a63
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-21 21:00:28 +0700

    modifikasi

---
+++ pertemuan 01/src/com/javacampus/Main.java
@@ -0,0 +1,9 @@
+package com.javacampus;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, welcome back");
+        System.out.println("I am a learning java");
+        System.out.println("It is a awesome");
+    }
+}
