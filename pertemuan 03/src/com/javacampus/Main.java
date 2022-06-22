1eda4bf3f278961b1f138ae39dbf78760415d3fe
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-22 07:31:19 +0700

    modifikasi

--- pertemuan 03/src/com/javacampus/Main.java
+++ pertemuan 03/src/com/javacampus/Main.java
@@ -1,14 +1,23 @@
 package com.javacampus;
 // constructor,class,object java
 //this is class
-class Student{
-    String name;
-    String address;
-    String major;
+class User{
+    String username;
+    String password;
+
+    User(String inputName, String inputPassword){
+        username = inputName;
+        password = inputPassword;
+
+        System.out.println("username:"+inputName);
+        System.out.println("password:"+inputPassword);
+    }
 }
 public class Main {
     public static void main(String[] args){
         //this istansiasi or object
+        User muhaemin = new User("muhaemin", "8308493");
+        User Rama = new User("Rama", "3848038");
 
     }
 
